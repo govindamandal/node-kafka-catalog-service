1fc1dfd31357d25eda84217522ff1ffb7c7f1e54
Govinda Mandal <govinda4india@gmail.com>
2024-12-22 01:29:24 +0530

    Added product and main routes

--- src/api/rest/catalog.route.ts
+++ src/api/rest/catalog.route.ts
@@ -3,7 +3,8 @@ import { NextFunction, Request, Response, Router } from "express";
 const catalogRouter = Router();
 
 catalogRouter.post('/product',async (request: Request, response: Response, next: NextFunction) => {
-    return response.status(201).json({});
+    console.log('Creating product');
+    return response.status(201).json({message: 'Product created'});
 })
 
 export default catalogRouter;
--- src/expressApp.ts
+++ src/expressApp.ts
@@ -4,6 +4,11 @@ import catalogRouter from './api/rest/catalog.route';
 const app = express();
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send({ message: 'Hello World!' });
+})
+
 app.use('/', catalogRouter);
 
 export default app;
