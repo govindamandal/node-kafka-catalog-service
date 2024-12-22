879b62555f6058163d94699eb4213df92e947d0e
Govinda Mandal <govinda4india@gmail.com>
2024-12-22 12:54:42 +0530

    Dummy commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import app from './expressApp';
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const StartServer = async () => {
     app.listen(PORT, () => {
